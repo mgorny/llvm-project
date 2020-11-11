6613a05c49f70108c111377c2dbd4e9842cee901
Michał Górny <mgorny@moritz.systems>
2020-11-11 12:43:04 +0100

    [lldb] [Process/FreeBSDRemote] Access FPR via RegisterInfo offsets
    Use offset-based method to access base x87 FPU registers, using offsets
    relative to the position of 'struct FPR', as determined by the location
    of first register in it (fctrl).  Change m_fpr to use a fixed-size array
    matching FXSAVE size (512 bytes).  Add unit tests for verifying
    RegisterInfo offsets and sizes against the FXSAVE layout.

--- lldb/unittests/Process/Utility/RegisterContextFreeBSDTests.cpp
+++ lldb/unittests/Process/Utility/RegisterContextFreeBSDTests.cpp
@@ -27,6 +27,10 @@ std::pair<size_t, size_t> GetRegParams(RegisterInfoInterface &ctx,
   return {info.byte_offset, info.byte_size};
 }
 
+#define ASSERT_OFF(regname, offset, size)                                      \
+  EXPECT_THAT(GetRegParams(reg_ctx, lldb_##regname),                           \
+              ::testing::Pair(offset + base_offset, size))
+
 #if defined(__x86_64__)
 
 #define ASSERT_GPR_X86_64(regname)                                             \
@@ -62,6 +66,57 @@ TEST(RegisterContextFreeBSDTest, x86_64) {
   ASSERT_GPR_X86_64(rflags);
   ASSERT_GPR_X86_64(rsp);
   ASSERT_GPR_X86_64(ss);
+
+  // fctrl is the first FPR field, it is used to determine offset of the whole
+  // FPR struct
+  size_t base_offset = reg_ctx.GetRegisterInfo()[lldb_fctrl_x86_64].byte_offset;
+
+  // assert against FXSAVE struct
+  ASSERT_OFF(fctrl_x86_64, 0x00, 2);
+  ASSERT_OFF(fstat_x86_64, 0x02, 2);
+  // TODO: This is a known bug, abridged ftag should is 8 bits in length.
+  ASSERT_OFF(ftag_x86_64, 0x04, 2);
+  ASSERT_OFF(fop_x86_64, 0x06, 2);
+  // NB: Technically fiseg/foseg are 16-bit long and the higher 16 bits
+  // are reserved.  However, we use them to access/recombine 64-bit FIP/FDP.
+  ASSERT_OFF(fioff_x86_64, 0x08, 4);
+  ASSERT_OFF(fiseg_x86_64, 0x0C, 4);
+  ASSERT_OFF(fooff_x86_64, 0x10, 4);
+  ASSERT_OFF(foseg_x86_64, 0x14, 4);
+  ASSERT_OFF(mxcsr_x86_64, 0x18, 4);
+  ASSERT_OFF(mxcsrmask_x86_64, 0x1C, 4);
+  ASSERT_OFF(st0_x86_64, 0x20, 10);
+  ASSERT_OFF(st1_x86_64, 0x30, 10);
+  ASSERT_OFF(st2_x86_64, 0x40, 10);
+  ASSERT_OFF(st3_x86_64, 0x50, 10);
+  ASSERT_OFF(st4_x86_64, 0x60, 10);
+  ASSERT_OFF(st5_x86_64, 0x70, 10);
+  ASSERT_OFF(st6_x86_64, 0x80, 10);
+  ASSERT_OFF(st7_x86_64, 0x90, 10);
+  ASSERT_OFF(mm0_x86_64, 0x20, 8);
+  ASSERT_OFF(mm1_x86_64, 0x30, 8);
+  ASSERT_OFF(mm2_x86_64, 0x40, 8);
+  ASSERT_OFF(mm3_x86_64, 0x50, 8);
+  ASSERT_OFF(mm4_x86_64, 0x60, 8);
+  ASSERT_OFF(mm5_x86_64, 0x70, 8);
+  ASSERT_OFF(mm6_x86_64, 0x80, 8);
+  ASSERT_OFF(mm7_x86_64, 0x90, 8);
+  ASSERT_OFF(xmm0_x86_64, 0xA0, 16);
+  ASSERT_OFF(xmm1_x86_64, 0xB0, 16);
+  ASSERT_OFF(xmm2_x86_64, 0xC0, 16);
+  ASSERT_OFF(xmm3_x86_64, 0xD0, 16);
+  ASSERT_OFF(xmm4_x86_64, 0xE0, 16);
+  ASSERT_OFF(xmm5_x86_64, 0xF0, 16);
+  ASSERT_OFF(xmm6_x86_64, 0x100, 16);
+  ASSERT_OFF(xmm7_x86_64, 0x110, 16);
+  ASSERT_OFF(xmm8_x86_64, 0x120, 16);
+  ASSERT_OFF(xmm9_x86_64, 0x130, 16);
+  ASSERT_OFF(xmm10_x86_64, 0x140, 16);
+  ASSERT_OFF(xmm11_x86_64, 0x150, 16);
+  ASSERT_OFF(xmm12_x86_64, 0x160, 16);
+  ASSERT_OFF(xmm13_x86_64, 0x170, 16);
+  ASSERT_OFF(xmm14_x86_64, 0x180, 16);
+  ASSERT_OFF(xmm15_x86_64, 0x190, 16);
 }
 #endif
 
@@ -98,5 +153,48 @@ TEST(RegisterContextFreeBSDTest, i386) {
   ASSERT_GPR_I386(esp);
   ASSERT_GPR_I386(ss);
   ASSERT_GPR_I386(gs);
+
+  // fctrl is the first FPR field, it is used to determine offset of the whole
+  // FPR struct
+  size_t base_offset = reg_ctx.GetRegisterInfo()[lldb_fctrl_i386].byte_offset;
+
+  // assert against FXSAVE struct
+  ASSERT_OFF(fctrl_i386, 0x00, 2);
+  ASSERT_OFF(fstat_i386, 0x02, 2);
+  // TODO: This is a known bug, abridged ftag should is 8 bits in length.
+  ASSERT_OFF(ftag_i386, 0x04, 2);
+  ASSERT_OFF(fop_i386, 0x06, 2);
+  // NB: Technically fiseg/foseg are 16-bit long and the higher 16 bits
+  // are reserved.  However, we use them to access/recombine 64-bit FIP/FDP.
+  ASSERT_OFF(fioff_i386, 0x08, 4);
+  ASSERT_OFF(fiseg_i386, 0x0C, 4);
+  ASSERT_OFF(fooff_i386, 0x10, 4);
+  ASSERT_OFF(foseg_i386, 0x14, 4);
+  ASSERT_OFF(mxcsr_i386, 0x18, 4);
+  ASSERT_OFF(mxcsrmask_i386, 0x1C, 4);
+  ASSERT_OFF(st0_i386, 0x20, 10);
+  ASSERT_OFF(st1_i386, 0x30, 10);
+  ASSERT_OFF(st2_i386, 0x40, 10);
+  ASSERT_OFF(st3_i386, 0x50, 10);
+  ASSERT_OFF(st4_i386, 0x60, 10);
+  ASSERT_OFF(st5_i386, 0x70, 10);
+  ASSERT_OFF(st6_i386, 0x80, 10);
+  ASSERT_OFF(st7_i386, 0x90, 10);
+  ASSERT_OFF(mm0_i386, 0x20, 8);
+  ASSERT_OFF(mm1_i386, 0x30, 8);
+  ASSERT_OFF(mm2_i386, 0x40, 8);
+  ASSERT_OFF(mm3_i386, 0x50, 8);
+  ASSERT_OFF(mm4_i386, 0x60, 8);
+  ASSERT_OFF(mm5_i386, 0x70, 8);
+  ASSERT_OFF(mm6_i386, 0x80, 8);
+  ASSERT_OFF(mm7_i386, 0x90, 8);
+  ASSERT_OFF(xmm0_i386, 0xA0, 16);
+  ASSERT_OFF(xmm1_i386, 0xB0, 16);
+  ASSERT_OFF(xmm2_i386, 0xC0, 16);
+  ASSERT_OFF(xmm3_i386, 0xD0, 16);
+  ASSERT_OFF(xmm4_i386, 0xE0, 16);
+  ASSERT_OFF(xmm5_i386, 0xF0, 16);
+  ASSERT_OFF(xmm6_i386, 0x100, 16);
+  ASSERT_OFF(xmm7_i386, 0x110, 16);
 }
 #endif
